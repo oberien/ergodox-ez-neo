7e7b33fc1f82af6ed8f3b53a8675c178910abd58
oberien <jaro.fietz@gmx.de>
2021-05-06 21:32:40 +0200

    Add buttons for mousewheel up and mousewheel down

--- src/my_keymap.h
+++ src/my_keymap.h
@@ -540,10 +540,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //                             │     │     │ --- │ │ --- │     │     │
 //                             └─────┴─────┴─────┘ └─────┴─────┴─────┘
   [F_MEDIA] = LAYOUT_ergodox(
-    KC_NO, KC_F1, KC_F2,   KC_F3,   KC_F4,    KC_F5, KC_F6,
-    KC_NO, KC_NO, KC_BTN1, KC_MS_U, KC_BTN2,  KC_NO, KC_NO,
-    KC_NO, KC_NO, KC_MS_L, KC_MS_D, KC_MS_R,  KC_NO,
-    KC_NO, KC_NO, KC_NO,   KC_NO,   KC_PAUSE, KC_NO, KC_NO,
+    KC_NO, KC_F1, KC_F2,   KC_F3,   KC_F4,    KC_F5,   KC_F6,
+    KC_NO, KC_NO, KC_BTN1, KC_MS_U, KC_BTN2,  KC_WH_U, KC_NO,
+    KC_NO, KC_NO, KC_MS_L, KC_MS_D, KC_MS_R,  KC_WH_D,
+    KC_NO, KC_NO, KC_NO,   KC_NO,   KC_PAUSE, KC_NO,   KC_NO,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_NO, KC_NO,
     KC_NO,
